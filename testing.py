e9f0e8b9cb3a1745b319d4893bff14122089a640
Victoria <57503137+victoriaaarmstrong@users.noreply.github.com>
2021-04-19 07:24:41 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -14,22 +14,22 @@ manualSeed = 999
 workers = 2
 batch_size = 64
 image_size = 64
-nc = 3
-nz = 100
-ngf = 64
-ndf = 64
+nc = 3 ## num channels, RGB
+nz = 100 ## latent vector size
+ngf = 64 ## num generator features
+ndf = 64 ## num discriminator features
 num_epochs = 100
-lr = 0.0002
+lr = 0.0002 ## learning rate
 beta1 = 0.5
 ngpu = 1 #for DL laptop
 wcm = 0.0 #weight mean initialization for convolutional layers
 wbm = 1.0 #weight mean initialization for batch norm layers
 ws = 0.02 #weight stf initilalzation for all layers
 bias = 0 #for weight initialization
-maxR = 1.2
-minR = 0.7
-maxF = 0.3
-minF = 0.0
+maxR = 1.2 ## for label smoothing
+minR = 0.7 ## for label smoothing
+maxF = 0.3 ## for label smoothing
+minF = 0.0 ## for label smoothing
 
 class Generator(nn.Module):
   def __init__(self, ngpu):
@@ -111,38 +111,43 @@ def main():
   ## Load Checkpoint
   device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
+  ## Instantiate model
   netG = Generator(ngpu).to(device)
   netD = Discriminator(ngpu).to(device)
 
+  ## Add optimizer
   optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
   optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
 
-  checkpoint = torch.load("./checkpoints/8_4000_train4") #0_0_train4")
+  #checkpoint = torch.load("./checkpoints/checkpoint_first_epoch")
+  #checkpoint = torch.load("./checkpoints/checkpoint_epoch_50")
+  checkpoint = torch.load("./checkpoints/checkpoint_final_epoch")
 
+  ## Output metrics
   loss_G = checkpoint['loss_generator']
   loss_D = checkpoint['loss_discriminator']
   epoch = checkpoint['epoch']
   D_x = checkpoint['D_x']
   D_G_z = checkpoint['D_G_z']
 
+  ## Load the models from the checkpoints
   netG.load_state_dict(checkpoint['generator_state_dict'])
   optimizerG.load_state_dict(checkpoint['optimizerG_state_dict'])
 
   netD.load_state_dict(checkpoint['discriminator_state_dict'])
   optimizerD.load_state_dict(checkpoint['optimizerD_state_dict'])
 
-  ## Run once with training and one with testing (w/in sample and outside sample)
-  #train_data = torch.load('./dataloader/train_train4')
-  test_data = torch.load('./dataloaders/test_train4')
+  ## Training and Testing Data
+  #train_data = torch.load('./dataloader/train_dataloader')
+  test_data = torch.load('./dataloaders/test_dataloader')
 
-  fake = netG(next(iter(test_data))[0]).detach().cpu()
-  img_list = vutils.make_grid(fake, padding=2, normalize=True)
 
   ctr = 0
   total = 0
   real_image_list = []
   fake_image_list =[]
 
+  ## Iterate through the testing data and save the real and fake data
   for i, (real, sparse) in enumerate(test_data, 0):
     real_cpu = real.to(device)
     fake = netG(sparse)
@@ -152,6 +157,7 @@ def main():
 
     ctr += 1
 
+  ## Save images of the real batches
   ctr = 0
   for i in real_image_list:
     fig = plt.figure(figsize=(15, 15))
@@ -160,6 +166,7 @@ def main():
     plt.close()
     ctr += 1
 
+  ## Save images of the fake generated batches
   ctr = 0
   for i in fake_image_list:
     fig = plt.figure(figsize=(15, 15))
